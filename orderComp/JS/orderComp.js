e53f7a68276db54d7a1444706f753c1b59d4ba67
20jy0112 <20jy0112@jec.ac.jp>
2022-02-03 12:28:06 +0900

    注文完了画面

---
+++ orderComp/JS/orderComp.js
@@ -0,0 +1,10 @@
+var
+var count = 0;
+
+let count = 3;
+const countUp = () =>{
+    colsole.log(count++);
+}
+
+setTimeout(countUp,300);
+window.history.go("../../Menu.php.index2.php");
---
+++ orderComp/PHP/orderComp.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>メインメニュー</title>
+    <link href="../CSS/orderComp.css" rel="stylesheet" type="text/css">
+    
+
+
+</head>
+<body>
+  
+	<header>
+        <h1 class="logo"><img src="../../img2/Logo.jpg" alt="sagamiya" width="450" height="100"></h1>
+    </header>
+    <div id ="wrapper" clickaction()>
+    <h1 class="Tenin"><img src="../../img2/Tenin1.jpeg" alt="sagamiya" width="150" height ="200"></h1>
+    <h3 class="table-text">テーブル番号札を持ってレジに向かってください。</h2>
+    
+    <p>
+    ご来店ありがとうございました。
+    </p>
+    またのお越しをお待ちしております。
+    </p>
+
+    </div>
+
+    <main>
+        
+     
+    </main>
+    <footer class="wrap">
+		<p><small>&copy;Copyright SAGAMIYA All rights reserved.</small>
+		<p>
+	</footer>
+    <link href="../JS/orderComp.js" rel="stylesheet">
+</body>
+</html>
